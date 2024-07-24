f74ddb57c228c75fc57732c5e2fac5288e8ee2a8
Jessica Olivares <rosewell@live.cl>
2024-07-24 21:28:04 +0200

    started build

--- front-end/src/components/PageThumbs.vue
+++ front-end/src/components/PageThumbs.vue
@@ -2,15 +2,21 @@
     <div class="flex justify-evenly items-center w-[100%] h-[22rem] my-[4rem] ">
         <router-link to="/today" class="h-[80%] w-[20rem] flex flex-col p-4 bg-white/10">
             <div class="p-2 mb-2 text-center text-white font-bold bg-[#bb345c]">See photos of the day</div>
-            <div class="w-full h-full bg-blue-400/40"></div>
+            <div class="w-full h-[80%]">
+                <img src="../assets/1.png" alt="" class="h-full w-full object-contain">
+            </div>
         </router-link>
         <router-link to="/today" class="h-[80%] w-[20rem] flex flex-col p-4 bg-white/10">
             <div class="p-2 mb-2 text-center text-white font-bold bg-[#bb345c]">Open by calendar</div>
-            <div class="w-full h-full bg-blue-400/40"></div>
+            <div class="w-full h-[80%]">
+                <img src="../assets/2.png" alt="" class="h-full w-full object-contain">
+            </div>
         </router-link>
         <router-link to="/gallery" class="h-[80%] w-[20rem] flex flex-col p-4 bg-white/10">
             <div class="p-2 mb-2 text-center text-white font-bold bg-[#bb345c]">Curated gallery</div>
-            <div class="w-full h-full bg-blue-400/40"></div>
+            <div class="w-full h-[80%]">
+                <img src="../assets/3.png" alt="" class="h-full w-full object-contain">
+            </div>
         </router-link>
 
 
