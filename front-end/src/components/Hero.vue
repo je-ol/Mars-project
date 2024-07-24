3e3c8070b225b8db558fe741348b4e2dc2cfdad7
Jessica Olivares <rosewell@live.cl>
2024-07-24 13:22:45 +0200

    fetching of news is working and styled article cards

--- front-end/src/components/Hero.vue
+++ front-end/src/components/Hero.vue
@@ -1,28 +1,25 @@
 <template>
-    <div class="carousel relative w-full h-[30rem] overflow-hidden">
+    <div class="carousel relative w-full h-[30rem] my-[4rem] overflow-hidden">
       <div
         class="carousel-item absolute w-full h-full transition-opacity duration-500"
         v-for="(slide, index) in slides"
         :key="index"
         :class="getClass(index, slide.bg)">
-
-        <!-- <div class="carousel-caption absolute bottom-5 left-5 bg-black bg-opacity-50 text-white p-4">
-          <h3>{{ slide.title }}</h3>
-          <p>{{ slide.description }}</p>
-        </div> -->
+        
         <div class="flex items-center">
+          <!-- Text content -->
           <div class="w-[40%] mt-[4rem] ml-[10rem] text-white">
             <h1 class="text-[4rem]">{{ slide.title }}</h1>
             <p class="my-2"> {{ slide.description }}</p>
             <button class="py-1 px-4 my-[1rem] rounded-sm border-white border-2 hover:bg-white hover:text-[#040056] hover:font-bold">LEARN MORE</button>
           </div>
-
+          <!-- Image content -->
           <div class="h-full w-[40%] absolute left-[68%] top-0">
             <img :src="slide.image" alt="" class="w-full max-h-full mb-6 object-cover" :style="{ opacity: slide.opacity }">
           </div>
         </div>
       </div>
-  
+      <!-- Slide controls -->
       <button class="carousel-control-prev absolute top-[50%] left-4 transform -translate-y-[50%] bg-black/20 text-white px-4 py-[3%] text-3xl rounded-full" @click="prevSlide">‹</button>
       <button class="carousel-control-next absolute top-[50%] right-4 transform -translate-y-[50%] bg-black/20 text-white px-4 py-[3%] text-3xl rounded-full" @click="nextSlide">›</button>
     </div>
@@ -49,7 +46,7 @@ export default {
         },
         {
           bg: 'bg-white/0',
-          title: 'CURRENT STATUS',
+          title: 'CURRENTLY',
           description: 'Mars exploration is advancing rapidly, highlighted by NASA\'s Perseverance rover, which landed in 2021 to search for ancient life and collect samples. The mission includes the Ingenuity helicopter, demonstrating powered flight on Mars. ESA\'s ExoMars Orbiter studies atmospheric gases, and China\'s Tianwen-1 mission, with an orbiter, lander, and rover, contributes to the international effort to explore the Red Planet.',
           image: current,
           opacity: 0.8
--- front-end/src/components/LatestNews.vue
+++ front-end/src/components/LatestNews.vue
@@ -3,11 +3,18 @@
         <div class="h-[14%] w-[10rem] items-start">
             <h2 class="text-white text-[2rem]">Latest News</h2>
         </div>
-        <div class="w-[2px] h-full bg-white/80 mx-4"></div>
-        <div class="flex justify-evenly items-end h-[100%] w-[100%]">
-            <div class="h-[86%] w-[20%] bg-white/30"></div>
-            <div class="h-[86%] w-[20%] bg-white/30"></div>
-            <div class="h-[86%] w-[20%] bg-white/30"></div>
+        <div class="w-[2px] h-full bg-white/80 mx-4 "></div>
+            <div class="flex justify-evenly items-end h-[100%] w-[100%]">
+                <div class="h-[86%] w-[20%] bg-white/95 shadow-lg rounded-sm"
+                v-for="(article, index) in news">
+                <img :src="article.image_url" alt="" class="h-[60%] w-full object-cover opacity-80">
+                <div class="h-[40%] w-full p-[0.5rem] shadow-inner">
+                    <h3 class="font-semibold">{{ article.title }}</h3>
+                    <h4 class="text-[0.75rem] my-[0.5rem]">{{ article.description }}</h4>
+                </div>
+
+            </div>
+
         </div>
 
     </div>
@@ -15,19 +22,23 @@
 
 <script setup>
 import axios from 'axios';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
     const news = ref([])
     const fetchNews = async () => {
         try {
-            const response = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=FxWVDl0pL3HYyTv3uTavGSv08uYKG5OU07zRJwZZ&search=mars', {
+            const response = await axios.get('https://api.thenewsapi.com/v1/news/all?api_token=FxWVDl0pL3HYyTv3uTavGSv08uYKG5OU07zRJwZZ&search=mars+nasa&language=en&exclude_domains=app.buzzsumo.com,ecns.cn', {
 
             })
+            news.value = response.data.data
         } catch (e) {
             console.log('Error fetching news', e)
         }
-
     }
+    onMounted( async () => {
+        await fetchNews()
+        console.log(news.value)
+    })
     
 </script>
 
